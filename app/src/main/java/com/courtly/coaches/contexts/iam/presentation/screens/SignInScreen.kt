8a2ef9c6a73a62835532b892aeff1f8c8bc326bd
Juan Angulo <68631508+Sve-nnN@users.noreply.github.com>
2026-06-30 22:25:19 -0500

    fix(ui): aplicar statusBarsPadding en pantalla de login

--- app/src/main/java/com/courtly/coaches/contexts/iam/presentation/screens/SignInScreen.kt
+++ app/src/main/java/com/courtly/coaches/contexts/iam/presentation/screens/SignInScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
@@ -76,6 +77,7 @@ fun SignInScreen(
         modifier = Modifier
             .fillMaxSize()
             .background(Background)
+            .statusBarsPadding()
             .imePadding()
     ) {
         Column(
